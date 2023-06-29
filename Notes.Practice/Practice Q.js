aa8a1a999bf7dfd89172952d930ff2c29c55ca77
Emily Svetcos <emilysvetcos@gmail.com>
2023-06-29 14:00:41 -0500

    bomb diffuse exercise

--- Notes.Practice/Practice Q.js
+++ Notes.Practice/Practice Q.js
@@ -315,65 +315,136 @@
 //
 //     Example:
 
+//
+// const users = [
+//
+//     {
+//         username: 'fred123',
+//         password: 'letmein',
+//         email: 'fred123@email.com',
+//         isAdmin: true
+//     },
+//     {
+//         username: 'cindy123',
+//         password: 'hello123',
+//         email: 'cindy123@email.com',
+//         isAdmin: false
+//     },
+//     {
+//         username: 'kathy123',
+//         password: 'letmein',
+//         email: 'kathy123@email.com',
+//         isAdmin: true
+//     },
+//     {
+//         username: 'kyle123',
+//         password: 'letmein',
+//         email: 'kyle123@email.com',
+//         isAdmin: false
+//     }
+// ]
+//
+// function returnAdmins(users) {
+//     const admins = [];
+//     for (let i = 0; i < users.length; i += 1) {
+//         if (users[i].isAdmin) {
+//             admins.push(users[i]);
+//         }
+//     }
+//     return admins;
+// }
+//
+// console.log(returnAdmins(users));
+//
+// /*
 
-const users = [
+// returnAdmins(users) returns...
+//
+// [
+//
+//     {
+//         username: 'fred123',
+//         password: 'letmein',
+//         email: 'fred123@email.com',
+//         isAdmin: true
+//     },
+//     {
+//         username: 'kathy123',
+//         password: 'letmein',
+//         email: 'kathy123@email.com',
+//         isAdmin: true
+//     }
+// ]
+//
+// */
+//
+// ================================= WARM UP
 
-    {
-        username: 'fred123',
-        password: 'letmein',
-        email: 'fred123@email.com',
-        isAdmin: true
-    },
-    {
-        username: 'cindy123',
-        password: 'hello123',
-        email: 'cindy123@email.com',
-        isAdmin: false
-    },
-    {
-        username: 'kathy123',
-        password: 'letmein',
-        email: 'kathy123@email.com',
-        isAdmin: true
-    },
-    {
-        username: 'kyle123',
-        password: 'letmein',
-        email: 'kyle123@email.com',
-        isAdmin: false
-    }
-]
+// Write a function that takes a neighborhood object and determines if it is desirable. A neighborhood is desirable.
+//     If the median home price is less than 300000, crime rates are low, and the total rating of schools is at least 24.
+// The function should return true if the neighborhood object is desireable and false otherwise.
 
-function returnAdmins(users) {
-    const admins = [];
-    for (let i = 0; i < users.length; i += 1) {
-        if (users[i].isAdmin) {
-            admins.push(users[i]);
-        }
-    }
-    return admins;
+// example data...
+
+    const neighborhood1 = {
+    neighborhood: "Lovely Estates",
+    medianHomePrice: 280000,
+    pool: true,
+    tennis: false,
+    crimeRate: "low",
+    schools: 22
+};
+
+const neighborhood2 = {
+    neighborhood: "Luminous Estates",
+    medianHomePrice: 270000,
+    pool: true,
+    tennis: false,
+    crimeRate: "high",
+    schools: 24
 }
 
-console.log(returnAdmins(users));
+const neighborhood3 = {
+    neighborhood: "Oak Mountain",
+    medianHomePrice: 290000,
+    pool: false,
+    tennis: false,
+    crimeRate: "low",
+    schools: 24
+}
 
-/*
+const neighborhood4 = {
+    neighborhood: "Ginormous Acres",
+    medianHomePrice: 350000,
+    pool: true,
+    tennis: true,
+    crimeRate: "low",
+    schools: 27
+}
 
-returnAdmins(users) returns...
+function goodNeighborhoods (neighborhood) {
+    // console.log(neighborhood.medianHomePrice);
+    // console.log(neighborhood.crimeRate);
+    // console.log(neighborhood.schools);
 
-[
+    const isRightPrice= neighborhood.medianHomePrice < 300000;
+    const isRightCrimeRate = neighborhood.crimeRate === 'low';
+    const isRightSchool= neighborhood.schools >= 24;
 
-    {
-        username: 'fred123',
-        password: 'letmein',
-        email: 'fred123@email.com',
-        isAdmin: true
-    },
-    {
-        username: 'kathy123',
-        password: 'letmein',
-        email: 'kathy123@email.com',
-        isAdmin: true
-    }
-]
+    const isDesireable = isRightCrimeRate && isRightSchool && isRightPrice;
 
-*/
+    return isDesireable;
+}
+
+// OR
+
+// function goodNeighborhoods (neighborhood) {
+//   return neighborhood.medianHomePrice < 300000 &&
+//          neighborhood.crimeRate === 'low' &&
+//          neighborhood.schools >= 24;
+//      }
+
+console.log(goodNeighborhoods(neighborhood1));
+console.log(goodNeighborhoods(neighborhood2));
+console.log(goodNeighborhoods(neighborhood3));
+console.log(goodNeighborhoods(neighborhood4));
