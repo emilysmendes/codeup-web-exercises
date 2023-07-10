da006ae69523b33d8435415f407549b9428213c8
Emily Svetcos <emilysvetcos@gmail.com>
2023-07-10 11:50:04 -0500

    mouse events exercise

--- js/jquery.js
+++ js/jquery.js
@@ -17,10 +17,53 @@
 //
 //
 
-$('li').css('font-size', '20px');
+// $('li').css('font-size', '20px');
+//
+// $('p').css('background color', 'yellow')
+// $('h1').css('background color', 'yellow')
+// $('li').css('background color', 'yellow')
+//
+// alert($('li').text());
 
-$('p').css('background color', 'yellow')
-$('h1').css('background color', 'yellow')
-$('li').css('background color', 'yellow')
 
-alert($('li').text());
+
+$('h1').click (
+    function () {
+        $(this).css('background-color', 'yellow')
+    }
+)
+
+                    //              PREFERRED METHOD        //
+// $('h1').on ('click', (e) => (
+//     $(e.target).css('background', 'yellow');
+// });
+
+
+$('p').dblclick(
+    function () {
+        $(this).css('font-size', '18px')
+    }
+)
+
+                //              PREFERRED METHOD        //
+// $('p').on('dblclick', () => (
+//     $('p').css('font-size', '18px');
+// });
+
+$('li').hover (
+    function () {
+        $(this).css('color', 'red');
+    },
+    function () {
+        $(this).css('color', 'black');
+    }
+)
+
+               //              PREFERRED METHOD        //
+// $('li').on('mouseenter', (e) => (
+//     $(e.target).css('color', 'red');
+//     });
+
+// $('li').on('mouseleave', (e) => (
+//     $(e.target).css('color', 'black');
+//     });
