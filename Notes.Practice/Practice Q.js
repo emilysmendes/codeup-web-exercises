332f247ea1a23041a77f486edc5045f9d0419a49
Emily Svetcos <emilysvetcos@gmail.com>
2023-07-10 09:04:18 -0500

    adding jquery practices

--- Notes.Practice/Practice Q.js
+++ Notes.Practice/Practice Q.js
@@ -474,7 +474,22 @@
 // }
 //
 // console.log(highestStudentCount); // returns 30
-
-ADDITIONAL EXAMPLE:
-
-function returnLargestStudentCount ()
+//
+// ADDITIONAL EXAMPLE:
+//
+// function returnLargestStudentCount ()
+// function fizzbuzz () {
+//     for (let i = 1; i < 100; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('fizzbuzz')
+//         } else if (i % 5 === 0) {
+//             console.log('buzz')
+//         } else if (i % 3 === 0) {
+//             console.log('fizz')
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+//
+// fizzbuzz();
---
+++ js/jquery.js
@@ -0,0 +1,26 @@
+// window.onload = function() {
+//     alert( 'The page has finished loading!' );
+// }
+
+// $(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+//
+//      alert($("#header-welcome").html());
+//      // const paragraph = $('#')
+//
+// $('.container').css('border', '1px dotted red');
+//      const codeup= document.querySelectorAll('.codeup');
+//      codeup.forEach( codeupStudents=> {
+//     codeup.style.border = '1px dotted red';
+// });
+//
+//
+
+$('li').css('font-size', '20px');
+
+$('p').css('background color', 'yellow')
+$('h1').css('background color', 'yellow')
+$('li').css('background color', 'yellow')
+
+alert($('li').text());
