c85371f8c0eedffe9ef62234e461d3c774be7931
Emily Svetcos <emilysvetcos@gmail.com>
2023-07-13 13:39:36 -0500

    ajax exercises

--- js/jquery.js
+++ js/jquery.js
@@ -69,58 +69,58 @@
 //     });
 
 
-// "use strict";
-//
-//  // BODY //
-// $('body')
-//     .css('background-image', "url('../css/img/matrix image.gif')")
-//     .css('background-repeat', 'repeat')
-//     .css('background-size', 'cover');
-//
-// // TEXT
-// $('#main-header, #main-text, #instructions')
-//     .css('text-align', 'center')
-//     .css('font-family', 'IBM Plex Mono')
-//     .css('color', '#ffffff')
-//     .css('margin-top', '80px');
-//
-// //HIDDEN MESSAGE
-// $('#hidden-message').hide()
-//
-//
-// $(document).keyup(function(event){
-//     console.log(event.keyCode);
-// });
-//
-// function onKonamiCode(cb) {
-//     var input = '';
-//     var key = '3838404037393739666513';
-//     document.addEventListener('keydown', function (e) {
-//         input += ("" + e.keyCode);
-//         if (input === key) {
-//             return cb();
-//         }
-//         if (!key.indexOf(input)) return;
-//         input = ("" + e.keyCode);
-//     });
-// }
-//
-// onKonamiCode(function () {
-//     alert('You gained 30 lives!')
-//     $('body')
-//         .css('background-image', "url('../css/img/cat matrix.jpeg')")
-//         .css('object-fit', 'cover')
-//         .css('background-size', 'cover')
-//         .css('background-repeat', 'no-repeat');
-//
-//
-//     $('#main-header, #main-text, #instructions').hide()
-//     $('#hidden-message').show()
-//         .css('text-align', 'center')
-//         .css('font-family', 'IBM Plex Mono')
-//         .css('color', '#ffffff')
-//         .css('margin-top', '80px');
-// });
+"use strict";
+
+ // BODY //
+$('body')
+    .css('background-image', "url('../css/img/matrix image.gif')")
+    .css('background-repeat', 'repeat')
+    .css('background-size', 'cover');
+
+// TEXT
+$('#main-header, #main-text, #instructions')
+    .css('text-align', 'center')
+    .css('font-family', 'IBM Plex Mono')
+    .css('color', '#ffffff')
+    .css('margin-top', '80px');
+
+//HIDDEN MESSAGE
+$('#hidden-message').hide()
+
+
+$(document).keyup(function(event){
+    console.log(event.keyCode);
+});
+
+function onKonamiCode(cb) {
+    var input = '';
+    var key = '3838404037393739666513';
+    document.addEventListener('keydown', function (e) {
+        input += ("" + e.keyCode);
+        if (input === key) {
+            return cb();
+        }
+        if (!key.indexOf(input)) return;
+        input = ("" + e.keyCode);
+    });
+}
+
+onKonamiCode(function () {
+    alert('You gained 30 lives!')
+    $('body')
+        .css('background-image', "url('../css/img/cat matrix.jpeg')")
+        .css('object-fit', 'cover')
+        .css('background-size', 'cover')
+        .css('background-repeat', 'no-repeat');
+
+
+    $('#main-header, #main-text, #instructions').hide()
+    $('#hidden-message').show()
+        .css('text-align', 'center')
+        .css('font-family', 'IBM Plex Mono')
+        .css('color', '#ffffff')
+        .css('margin-top', '80px');
+});
 
 // "use strict"nlk
 
@@ -147,28 +147,28 @@
 //     $(this).toggleClass('highlighted')
 // })
 
-//                  HOW TO DO IT IN JS (VANILLA JAVASCRIPT)
-const hideContent = document.createElement('a');
-hideContent.innerText = 'Space Magic';
-
-const bodyElement = document.querySelector('body');
-bodyElement.appendChild(hideContent);
-
-const ddElements = document.querySelectorAll('dd');
-hideContent.addEventListener('click', ()=> {
-    ddElements.forEach((ddElements) => {
-        ddElements.classList.toggle('invisible');
-    });
-});
-
-const dtElements = document.querySelectorAll('dt');
-dtElements.forEach((dtElement) => {
-    dtElement.addEventListener('click', () => {
-        dtElement.classList.toggle('highlighted')
-    });
-});
-
-
+// //                  HOW TO DO IT IN JS (VANILLA JAVASCRIPT)
+// const hideContent = document.createElement('a');
+// hideContent.innerText = 'Space Magic';
+//
+// const bodyElement = document.querySelector('body');
+// bodyElement.appendChild(hideContent);
+//
+// const ddElements = document.querySelectorAll('dd');
+// hideContent.addEventListener('click', ()=> {
+//     ddElements.forEach((ddElements) => {
+//         ddElements.classList.toggle('invisible');
+//     });
+// });
+//
+// const dtElements = document.querySelectorAll('dt');
+// dtElements.forEach((dtElement) => {
+//     dtElement.addEventListener('click', () => {
+//         dtElement.classList.toggle('highlighted')
+//     });
+// });
+//
+//
 
 
 
@@ -180,20 +180,20 @@ dtElements.forEach((dtElement) => {
 //     When any list item is clicked, first li of the parent ul should have a font color of blue.
 
                             // JQUERY //
-$(() => {
-    // $('body').append(
-    //     `<button>Highlight</button>`
-    //  )
-    $('#yellow-btn').on('click', function () {
-        $('ul').each(function (index, ul) {
-            $(ul).children().last().css('background-color', 'yellow')
-        })
-    })
-    $('h3').on('click', function () {
-        $(this).next('ul').children().css('font-weight', 'bold');
-    })
-    $('li').on('click', function () {
-        $(this).parent().children().first().css('color', 'blue')
-    });
-})
+// $(() => {
+//     // $('body').append(
+//     //     `<button>Highlight</button>`
+//     //  )
+//     $('#yellow-btn').on('click', function () {
+//         $('ul').each(function (index, ul) {
+//             $(ul).children().last().css('background-color', 'yellow')
+//         })
+//     })
+//     $('h3').on('click', function () {
+//         $(this).next('ul').children().css('font-weight', 'bold');
+//     })
+//     $('li').on('click', function () {
+//         $(this).parent().children().first().css('color', 'blue')
+//     });
+// })
 
